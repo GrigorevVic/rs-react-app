bd3ccd48be19a72460df394a4d486f903916e1a3
Grigorev Viktor <4342718@gmail.com>
2024-12-18 17:34:12 +0300

    feat: replacing the SearchForm class by a function

--- src/components/main/searchForm/SearchForm.tsx
+++ src/components/main/searchForm/SearchForm.tsx
@@ -1,61 +1,47 @@
 import './styles.css';
-import { Component } from 'react';
-
-interface SearchState {
-  search: string;
-  hasError: boolean;
-}
+import { useState } from 'react';
 
 interface SearchProps {
   handleSearch: (search: string) => void;
 }
 
-export class SearchForm extends Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    const savedSearch = localStorage.getItem('searchString') || '';
-    this.state = {
-      search: savedSearch,
-      hasError: false,
-    };
-    this.props.handleSearch(this.state.search);
-  }
+export function SearchForm(props: SearchProps) {
+  const savedSearch = localStorage.getItem('searchString') || '';
+  const [search, setSearch] = useState({ search: savedSearch });
+  const [hasError, setError] = useState({ hasError: false });
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ search: event.target.value.trim() });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearch({ search: event.target.value.trim() });
   };
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    localStorage.setItem('searchString', this.state.search);
-    this.props.handleSearch(this.state.search);
+    localStorage.setItem('searchString', search.search);
+    props.handleSearch(search.search);
   };
 
-  getError = () => {
-    this.setState({ hasError: true });
+  const getError = () => {
+    setError({ hasError: true });
   };
 
-  render() {
-    const { hasError } = this.state;
-    if (hasError) {
-      throw new Error('An error has occurred');
-    }
-    return (
-      <form onSubmit={this.handleSubmit} className="search-form" name="form">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Enter a character name..."
-          value={this.state.search}
-          onChange={this.handleChange}
-        />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-        <button type="submit" className="error-button" onClick={this.getError}>
-          Error
-        </button>
-      </form>
-    );
+  if (hasError.hasError) {
+    throw new Error('An error has occurred');
   }
+  return (
+    <form onSubmit={handleSubmit} className="search-form" name="form">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Enter a character name..."
+        value={search.search}
+        onChange={handleChange}
+      />
+      <button type="submit" className="search-button">
+        Search
+      </button>
+      <button type="submit" className="error-button" onClick={getError}>
+        Error
+      </button>
+    </form>
+  );
 }
