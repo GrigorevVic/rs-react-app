f0ee661ee36a25676d898c9c08342b94f267040d
Grigorev Viktor <4342718@gmail.com>
2025-02-13 20:45:04 +0300

    refactor: component 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,8 @@
-import './styles.css';
-import { Link } from 'react-router-dom';
-import { Header } from '../../components/header/Header';
-import { Footer } from '../../components/footer/Footer';
+import Link from 'next/link';
+import { Header } from '../components/header/Header';
+import { Footer } from '../components/footer/Footer';
 
-export function NotFoundPage() {
+export default function Page404() {
   return (
     <>
       <Header />
@@ -11,7 +10,7 @@ export function NotFoundPage() {
         <div className="wrapper-404">
           <p>Page not found</p>
           <h1 className="title-404">404</h1>
-          <Link to="/">
+          <Link href="/">
             <button className="btn-404">Go Home</button>
           </Link>
         </div>
